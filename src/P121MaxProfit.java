31781209d4412884dcb17950101e1b494cf32d56
spanme <asitsn9ine@gmail.com>
2025-02-25 19:17:01 +0000

    Problem121, Max Profit.

---
+++ src/P121MaxProfit.java
@@ -0,0 +1,24 @@
+public class P121MaxProfit {
+    public int maxProfit(int[] prices) {
+        //prices [i] is the price on the ith day
+        //want to buy at lowest and sell at highest
+        //return profit
+        //however, buying has to be before selling.
+
+        if (prices.length == 0) {
+            return 0;
+        }
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price; //if you find a smaller price, you shouldnt sell on this day
+            } else if (price - minPrice > maxProfit) { //so we can go to an elif
+                maxProfit = price - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
